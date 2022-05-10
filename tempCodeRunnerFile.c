67539b5b0f51d874d3b7b835ab8b596c4c139981
Ravi Raghavan <ravi0308raghavan@gmail.com>
2022-05-10 09:21:54 -0400

    updated makefile

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,1 @@
-fprintf(fptr, "\nNumber of Prime Numbers Identified is: %d\n", primeNumbersIdentified);
-    fprintf(fptr, "Highest Prime Number Identified is: %d\n", highestPrimeNumberIdentified);
+sqrt
